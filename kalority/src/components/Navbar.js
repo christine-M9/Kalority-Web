5015e18327a4eb9c710756055c5e1d47c15d920d
Christine <christinekhekama99@gmail.com>
2024-06-17 02:54:03 +0300

    components

--- kalority/src/components/Navbar.js
+++ kalority/src/components/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">Kalority</div>
       <ul className="navbar-nav">
-        <li><a href="/">Home</a></li>
-        <li><a href="/details">Details</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/team">Team</a></li>
-        <li><a href="/success-stories">Success Stories</a></li>
-        <li><a href="/features">Features</a></li>
+        <li><a href="/">HOME</a></li>
+        <li><a href="/details">DETAILS</a></li>
+        <li><a href="/contact">CONTACT</a></li>
+        <li><a href="/team">TEAM</a></li>
+        <li><a href="/success-stories">SuCCESS STORIES</a></li>
+        <li><a href="/features">FEATURES</a></li>
       </ul>
     </div>
   </nav>

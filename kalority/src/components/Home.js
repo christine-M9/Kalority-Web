98feef09ca8b3f30aab0ae964646e3d80446ca1b
Christine <christinekhekama99@gmail.com>
2024-06-17 20:38:01 +0300

    homepage styling

--- kalority/src/components/Home.js
+++ kalority/src/components/Home.js
@@ -1,6 +1,5 @@
-// Home.js
 import React from 'react';
-import { useLocation } from 'react-router-dom'; // Import useLocation hook
+import { useLocation } from 'react-router-dom';
 import '../App.css';
 
 const Home = () => {
@@ -8,7 +7,17 @@ const Home = () => {
 
   return (
     <div className={`home-container ${location.pathname === '/' ? 'home-background' : ''}`}>
-      <h1>Welcome to Kalority</h1>
+      <div className="content-wrapper">
+        <h2>Welcome to Kalority</h2>
+        <div className="additional-content">
+          <p>
+            Where innovation meets excellence in software-driven solutions. We specialize in crafting
+            and maintaining bespoke software applications tailored to your unique needs. Our team is
+            dedicated to pushing the boundaries of technology to drive your business forward in the
+            digital landscape.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };

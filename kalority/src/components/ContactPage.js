b97126a3cc5c416a701b1d76d142161ed9f97635
Christine <christinekhekama99@gmail.com>
2024-06-18 19:42:37 +0300

    contactpage component

---
+++ kalority/src/components/ContactPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+const ContactPage = () => {
+  return (
+    <div className="contact-container">
+     <h2>Get in Touch</h2>
+      <form className="contact-form">
+        <input type="text" id="fullName" name="fullName" placeholder="Full name" />
+        <input type="email" id="email" name="email" placeholder="Email address" />
+        <textarea id="message" name="message" rows="4" placeholder="Message"></textarea>
+        <button type="submit" className="submit-button">Send Message</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;

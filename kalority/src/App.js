7143242b8923e3388a32f4dc128a1283e8c03851
Christine <christinekhekama99@gmail.com>
2024-08-14 09:45:32 +0300

    minor changes

--- kalority/src/App.js
+++ kalority/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Details from './components/Details';
 import Features from './components/Features';
 import Contact from './components/ContactPage';
-import Team from './components/TeamSection';
-import SuccessStories from './components/SuccessStories';
-import './App.css'; // Import your CSS file for styling
+// import Details from './components/Details';
+// import Team from './components/TeamSection';
+// import SuccessStories from './components/SuccessStories';
+import './App.css';
 
 const App = () => {
   return (
@@ -17,11 +17,11 @@ const App = () => {
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/details" element={<Details />} />
             <Route path="/features" element={<Features />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/successstories" element={<SuccessStories />} />
+             {/* <Route path="/details" element={<Details />} /> */}
+            {/* <Route path="/team" element={<Team />} /> */}
+            {/* <Route path="/successstories" element={<SuccessStories />} /> */}
           </Routes>
         </div>
       </div>

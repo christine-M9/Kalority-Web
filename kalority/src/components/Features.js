c9da00243f393837873acefde07d13afa35a61e7
Christine <christinekhekama99@gmail.com>
2024-08-16 23:53:09 +0300

    Adding 3Dimage view

--- kalority/src/components/Features.js
+++ kalority/src/components/Features.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../App.css"; 
 
 const Features = () => {
+  useEffect(() => {
+    const image = document.querySelector('.image-section img');
+
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
+      const deltaX = clientX - centerX;
+      const deltaY = clientY - centerY;
+
+      const rotateX = (deltaY / centerY) * 30;
+      const rotateY = (deltaX / centerX) * -30; 
+
+      image.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    };
+
+    const resetImagePosition = () => {
+      image.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseout', resetImagePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseout', resetImagePosition);
+    };
+  }, []);
+
   return (
     <div className="features">
       <div className="text-section">

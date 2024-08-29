251446de85773e5c56f9dddb0aa4767aac00584b
Christine <christinekhekama99@gmail.com>
2024-08-29 22:47:42 +0300

    testimonials scroll movement restricted when on testimonials page only

--- kalority/src/components/Testimonials.js
+++ kalority/src/components/Testimonials.js
@@ -26,7 +26,7 @@ const Testimonials = () => {
 
   useEffect(() => {
     const scrollInterval = setInterval(() => {
-      if (scrollRef.current) {
+      if (scrollRef.current && isInViewport(scrollRef.current)) {
         scrollRef.current.scrollBy({
           left: cardWidth,
           behavior: "smooth",
@@ -46,6 +46,18 @@ const Testimonials = () => {
       });
     }
     setActiveDot(index);
+  };
+
+  // Function to check if an element is in the viewport
+  const isInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
   };
 
   return (

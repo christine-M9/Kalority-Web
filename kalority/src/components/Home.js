74afdabf28f795a3dd02fe535ca7ad13a5728056
Christine <christinekhekama99@gmail.com>
2024-08-29 23:43:44 +0300

    homepage semicircle

--- kalority/src/components/Home.js
+++ kalority/src/components/Home.js
@@ -5,11 +5,8 @@ const Home = () => {
   return (
     <div id="home" className="home-container home-background">
       <div className="new-image-container">
-        <img
-          src="/images/Circle_Seal-brown_Solid.svg.png" 
-          alt="Semicircular Illustration"
-          className="semicircular-illustration"
-        />
+        {/* Removed the img tag and replaced it with a div for the semicircle */}
+        <div className="semicircular-illustration"></div>
       </div>
       <div className="content-wrapper">
         <h2>Social Media Marketing is the Best Ever</h2>
